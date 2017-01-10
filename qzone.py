3779491f1a8677b26fbb08287b0d4ba95a128e4f
miyazawatomoka <miyazawa_tomoka@yahoo.co.jp>
2017-01-11 00:32:21 +0800

    fix

--- qzone.py
+++ qzone.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # encoding=utf-8
 
-import urllib.request, json, time
+import urllib.request, json, time, http.cookiejar
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0'
 
