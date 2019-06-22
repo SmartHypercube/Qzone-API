3555673a4a69fcfae121bad954aa75f9941537bd
Hypercube <hypercube@0x01.me>
2019-06-23 04:54:36 +0800

    Update qzone.py

--- qzone.py
+++ qzone.py
@@ -219,7 +219,7 @@ class Emotion:
             with urllib.request.urlopen(req) as http:
                 s = http.read().decode()
             data['commentlist'] += json.loads(s[s.find('(')+1 : s.rfind(')')])['commentlist']
-        url = make_url('http://users.edu.qzone.qq.com/cgi-bin/likes/get_like_list_app',
+        url = make_url('http://users.qzone.qq.com/cgi-bin/likes/get_like_list_app',
                 uin = qzone_cookie['ptui_loginuin'],
                 unikey = 'http%%3A%%2F%%2Fuser.qzone.qq.com%%2F%s%%2Fmood%%2F%s' % (self.author, self.tid),
                 begin_uin = 0,
